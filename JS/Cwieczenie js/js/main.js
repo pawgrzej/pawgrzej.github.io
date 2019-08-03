8d6bd0ee5ee23cdc3375fb566a1fa432ec0e5a89
pawgrzej <pawel.grzejszczak90@gmail.com>
2019-08-03 12:46:26 +0200

    js
    js

---
+++ JS/Cwieczenie js/js/main.js
@@ -0,0 +1,45 @@
+
+let wynik;
+
+function dodawanie(x,y) {
+
+wynik=(x+y)*2;
+ 
+return wynik;
+
+}
+
+wynikFu = dodawanie(3,3)
+console.log(wynikFu)
+
+
+let odejmowanie = (a,b ) => {
+    let roznica;
+    roznica = a-b 
+    return roznica;
+}
+
+console.log(odejmowanie(50,18))
+
+
+let zadanie = (c,d) => {
+ 
+    let dzielenie;
+    dzielenie = c/d
+    return dzielenie;
+
+}
+
+console.log(zadanie(32,8))
+
+
+
+
+let backtick = 50
+let napis = `Ola ma ${backtick} lat`
+
+console.log(napis)
+
+
+
+
---
+++ JS/cwiczenie JS Wyplata/js/main.js
@@ -0,0 +1,31 @@
+
+
+let wyplata = (a,b) =>
+{
+    let dodawanie;
+    dodawanie= a+b;
+    return dodawanie;
+} 
+
+
+
+let backtick = wyplata(5000,2500)
+let backtick2 = 500000
+let napis= `Moja wypłata to ${backtick} a chciał bym zarabiać ${backtick2} `
+
+console.log(napis)
+
+
+
+let zadanie = (j,k,l) =>
+{
+    let matematyka;
+    matematyka=(50,30,20);
+    return matematyka
+}
+
+let info1 = zadanie(90*8)-30;
+let info2 = 900;
+
+let napis2= `Zabawa jeden ${info1} nie rowona sie zadania ${info2}`
+console.log(napis2)
