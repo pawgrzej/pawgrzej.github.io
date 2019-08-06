c629a0c0fc32fda40e3a137a628098df55917559
pawgrzej <pawel.grzejszczak90@gmail.com>
2019-08-06 19:15:24 +0200

    06
    06

--- Zadania Domowe/JS/6-obiekt-json/main.js
+++ Zadania Domowe/JS/6-obiekt-json/main.js
@@ -14,3 +14,21 @@ jasonPracownicy.pracownicy.forEach(function(element,index)
  {
     console.log([index] + '- Imie: '+ element.firstName+ ' Nazwisko: '+element.lastName);
 });
+
+
+
+
+// Pobierz do zmiennej jsonPracownicy poniższy obiekt:
+
+// {
+//     "pracownicy":[
+//         {"firstName": "Krystian", "lastName": "Dziopa"}, 
+//         {"firstName": "Anna", "lastName": "Szapiel"},
+//         {"firstName": "Piotr", "lastName": "Żmuda"}
+//     ]
+// }
+// Wyświetl go w konsoli, a następnie za pomocą pętli forEach przeiteruj po każdym pracowniku,
+//  oraz wyświetl w konsoli index elementu tablicy oraz imie i nazwisko pracownika.
+
+// Zadanie domowe robimy z wykorzystaniem serwisu https://repl.it
+
