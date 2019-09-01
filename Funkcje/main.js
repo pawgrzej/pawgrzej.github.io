464fbe084f9e85059dc2ff653ca216db6ea87b65
pawgrzej <pawel.grzejszczak90@gmail.com>
2019-09-01 09:28:36 +0200

    Wors press Nauka
    Word Press Nauka

---
+++ Funkcje/main.js
@@ -0,0 +1,18 @@
+// const btn = document.getElementById("przycisk");
+// const output = document.getElementById("output");
+
+// const array =
+
+// //Wstaw tres//
+
+// btn.addEventListener('click',() => (
+//     output.innerHTML = output.innerHTML + ' dodatkowa tresc po kliku';
+
+// ));
+
+
+
+const oprocentowanie = 1;
+const type ="dodawanie";
+ const działanie 
+
